f8c56c071a41158b202c3d89e61e695c2fd7080b
root <root@azerothcore.proxmox>
2026-08-25 18:00:49 +0200

    Revert "Avoid phase 00 item loot collisions"
    This reverts commit fd1a11237ae99e9073bf80e9ff8986921ae7c406.

--- src/phase_00/sql/phase_00-item_loot_template.sql
+++ src/phase_00/sql/phase_00-item_loot_template.sql
@@ -1,32 +1,11 @@
-DELETE old
-FROM `item_loot_template` AS old
-JOIN `item_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 40752
-WHERE old.`Entry` IN (43347, 52676) AND old.`Item` = 47241;
-
 UPDATE `item_loot_template` SET `Item` = 40752 WHERE `entry` IN ( -- Emblem of Heroism
     43347, -- Satchel of Spoils
     52676 -- Cache of the Ley-Guardian
 ) AND `Item` = 47241; -- Emblem of Triumph
-DELETE old
-FROM `item_loot_template` AS old
-JOIN `item_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 40753
-WHERE old.`Entry` IN (43346, 45875) AND old.`Item` = 47241;
-
 UPDATE `item_loot_template` SET `Item` = 40753 WHERE `entry` IN ( -- Emblem of Valor
     43346, -- Large Satchel of Spoils
     45875 -- Sack of Ulduar Spoils
 ) AND `Item` = 47241; -- Emblem of Triumph
-DELETE old
-FROM `item_loot_template` AS old
-JOIN `item_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 40753
-WHERE old.`Entry` = 45878 AND old.`Item` = 47241;
-
 UPDATE `item_loot_template` SET `Item` = 40753 WHERE `entry` IN ( -- Emblem of Conquest
     45878 -- Large Sack of Ulduar Spoils
 ) AND `Item` = 47241; -- Emblem of Triumph

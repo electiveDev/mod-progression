af9e70d618504997f7f1af2aa4383a5cc2620e31
root <root@azerothcore.proxmox>
2026-08-25 18:00:49 +0200

    Revert "Avoid phase 00 gameobject loot collisions"
    This reverts commit e20ab6a9c42ec3782d633be35cdd195e0b96f750.

--- src/phase_00/sql/phase_00-gameobject_loot_template.sql
+++ src/phase_00/sql/phase_00-gameobject_loot_template.sql
@@ -1,11 +1,3 @@
-DELETE old
-FROM `gameobject_loot_template` AS old
-JOIN `gameobject_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 40752
-WHERE old.`Entry` IN (24524, 24589, 25192, 26094, 26260)
-  AND old.`Item` = 47241;
-
 UPDATE `gameobject_loot_template` SET `Item` = 40752 WHERE `entry` IN ( -- Emblem of Heroism
     24524, -- Cache of Eregos
     24589, -- Dark Runed Chest
@@ -13,16 +5,6 @@ UPDATE `gameobject_loot_template` SET `Item` = 40752 WHERE `entry` IN ( -- Emble
     26094, -- Alexstrasza's Gift
     26260 -- Tribunal Chest
 ) AND `Item` = 47241; -- Emblem of Triumph
-DELETE old
-FROM `gameobject_loot_template` AS old
-JOIN `gameobject_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 40753
-WHERE old.`Entry` IN (25193, 26097, 26974, 27061, 27068, 27073, 27074,
-                      27078, 27079, 27080, 27081, 27085, 27086, 27414,
-                      27416, 27417)
-  AND old.`Item` = 47241;
-
 UPDATE `gameobject_loot_template` SET `Item` = 40753 WHERE `entry` IN ( -- Emblem of Valor
     25193, -- Four Horsemen Chest
     26097, -- Alexstrasza's Gift
@@ -41,15 +23,6 @@ UPDATE `gameobject_loot_template` SET `Item` = 40753 WHERE `entry` IN ( -- Emble
     27416, -- Eadric's Cache
     27417 -- Confessor's Cache
 ) AND `Item` = 47241; -- Emblem of Triumph
-DELETE old
-FROM `gameobject_loot_template` AS old
-JOIN `gameobject_loot_template` AS target
-  ON target.`Entry` = old.`Entry`
- AND target.`Item` = 45624
-WHERE old.`Entry` IN (26929, 26946, 26955, 26956, 26959, 26960, 26961,
-                      26962, 26963, 26967, 27030)
-  AND old.`Item` = 47241;
-
 UPDATE `gameobject_loot_template` SET `Item` = 45624 WHERE `entry` IN ( -- Emblem of Conquest
     26929, -- Cache of Living Stone
     26946, -- Cache of Winter
